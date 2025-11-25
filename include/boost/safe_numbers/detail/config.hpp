b7b195e8078b5d4d7163b18805ca84504ddb4d88
Matt Borland <matt@mattborland.com>
2025-11-25 14:21:12 +0100

    Add has_builtin config macro

--- include/boost/safe_numbers/detail/config.hpp
+++ include/boost/safe_numbers/detail/config.hpp
@@ -5,10 +5,19 @@
 #ifndef BOOST_SAFENUMBERS_CONFIG_HPP
 #define BOOST_SAFENUMBERS_CONFIG_HPP
 
+#include <boost/config.hpp>
+
 #ifdef BOOST_SAFE_NUMBERS_BUILD_MODULE
 #  define BOOST_SAFE_NUMBERS_EXPORT export
 #else
 #  define BOOST_SAFE_NUMBERS_EXPORT
 #endif
+
+#ifdef __has_builtin
+#  define BOOST_SAFE_NUMBERS_HAS_BUILTIN(x) __has_builtin(x)
+#else
+#  define BOOST_SAFE_NUMBERS_HAS_BUILTIN(x) 0
+#endif // __has_builtin
+
 
 #endif // BOOST_SAFENUMBERS_CONFIG_HPP
